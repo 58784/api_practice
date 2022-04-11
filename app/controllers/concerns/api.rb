a780006c92721c851e37828a54b80d4048fc9b9b
58784 <k56k.k-0504@docomo.ne.jp>
2022-04-11 19:24:44 +0900

    課題５rubocop再修正

--- app/controllers/concerns/api.rb
+++ app/controllers/concerns/api.rb
@@ -2,21 +2,21 @@ module Api
   extend ActiveSupport::Concern
 
   included do
-    rescue_from StandardError, with: :render_500
-    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from StandardError, with: :render500
+    rescue_from ActiveRecord::RecordNotFound, with: :render404
   end
 
   private
 
-  def render_400(exception = nil, messages = nil)
+  def render400(exception = nil, messages = nil)
     render_error(400, 'Bad Request', exception&.message, *messages)
   end
 
-  def render_404(exception = nil, messages = nil)
+  def render404(exception = nil, messages = nil)
     render_error(404, 'Record Not Found', exception&.message, *messages)
   end
 
-  def render_500(exception = nil, messages = nil)
+  def render500(exception = nil, messages = nil)
     render_error(500, 'Internal Server Error', exception&.message, *messages)
   end
 
