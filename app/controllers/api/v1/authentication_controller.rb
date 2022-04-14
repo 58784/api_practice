6c791c867908199314127847034a06e1daea67b7
58784 <k56k.k-0504@docomo.ne.jp>
2022-04-14 16:51:52 +0900

    lintcheck更新

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
 module Api
-    module V1
-      class AuthenticationController < BaseController
-        def create
-          @user = login(params[:email], params[:password])
-          if @user
-            json_string = UserSerializer.new(@user).serialized_json
-            render json: json_string
-          else
-            response = {
-              message: 'Record Not Found',
-              errors: ['ActiveRecord::RecordNotFound']
-            }
-            render json: response, status: 404
-          end
+  module V1
+    class AuthenticationController < BaseController
+      def create
+        @user = login(params[:email], params[:password])
+        if @user
+          json_string = UserSerializer.new(@user).serialized_json
+          render json: json_string
+        else
+          response = {
+            message: 'Record Not Found',
+            errors: ['ActiveRecord::RecordNotFound']
+          }
+          render json: response, status: 404
         end
-
-        private
-
-        def form_authenticity_token; end
       end
+
+      private
+
+      def form_authenticity_token; end
     end
+  end
 end
